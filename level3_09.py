78c679a7369d2df717900c20cfab03a62e863c7c
AleValleLazaro.prueba <alexvalaz@gmail.com>
2024-06-06 12:15:38 +0200

    Level 3 ejercicio 09

---
+++ level3_09.py
@@ -0,0 +1,5 @@
+# Ejercicio 9
+x = input("Dime tu fecha de cumpleaños en formato dd/mm/aaaa: ")
+y= x.split('/')
+
+print(f"Tu día de cumpleaños es {x.split('/')[0]}, tu mes es {x.split('/')[1]} y tu año es {x.split('/')[2]}")
