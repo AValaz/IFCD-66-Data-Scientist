1c2efe814d296a535df1a89695778ac78621dac9
AleValleLazaro.prueba <alexvalaz@gmail.com>
2024-06-06 10:06:24 +0200

    Level 1 ejercicio 10

---
+++ level1_10.py
@@ -0,0 +1,6 @@
+# Ejercicio 10
+
+x = int(input("Dime el nº de payasos del pedido: "))
+y = int(input("Dime el nº de muñecas del pedido: "))
+
+print(f"El peso total del pedido es {x*112 + y*75} gramos")
