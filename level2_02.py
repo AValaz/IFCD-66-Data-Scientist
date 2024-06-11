07d36acdd35ccf54d600333723f6b7773ce1c615
AleValleLazaro.prueba <alexvalaz@gmail.com>
2024-06-11 09:47:22 +0200

    añadimos level2_02.py

---
+++ level2_02.py
@@ -0,0 +1,16 @@
+## Ejercicio 2
+
+def calculadora (numero, func):
+    func = input("Dime que función quieres que aplique: ")
+    numero = input("Dime el valor entero: ")
+
+    if func == "seno":
+    
+    if func == "coseno":
+    
+    if func == "tangente":
+    
+    if func == "exponencial":
+    
+    if func == "logaritmo":
+
