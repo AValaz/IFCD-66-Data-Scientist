b836b5dbc30ea2a3cbfc9edc248013dbfb8090fa
AleValleLazaro.prueba <alexvalaz@gmail.com>
2024-06-13 18:12:38 +0200

    añadimos level5_08.py con cambios

---
+++ level5_08.py
@@ -0,0 +1,9 @@
+## ejercicio 8
+
+palabra = input("Dime una palabra: ")
+
+if palabra == palabra[::-1]:
+    print("Esta palabra es un palíndromo")
+else:
+    print("Lo siento, no es un palíndromo")
+    
