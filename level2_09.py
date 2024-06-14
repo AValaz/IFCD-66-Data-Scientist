9f8232bb4da4358744ce60763e376bdbe743a5eb
AleValleLazaro.prueba <alexvalaz@gmail.com>
2024-06-14 12:41:21 +0200

    añadimos level2_09.py con cambios del profesor

--- level2_09.py
+++ level2_09.py
@@ -7,3 +7,23 @@ def vector (*numbers):
 
     return suma
 print(vector(2,2,3,4))
+
+## Solución profesor 
+# from functools import reduce
+
+# def cuadrado(i,j):
+#     return i+j**2
+
+# def modulov1(v):
+#     l = [x**2 for x in v]
+#     c = 0 
+#     for k in l:
+#         c+=k
+#     return c
+
+# def modulo_v2(v):
+#     return reduce(cuadrado, v, 0) ** 0.5
+
+# print(modulo((2,2,3)))
+
+
