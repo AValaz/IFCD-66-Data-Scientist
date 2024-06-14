ea9270a00e7393d539e595d66d7e375804a88c77
AleValleLazaro.prueba <alexvalaz@gmail.com>
2024-06-14 13:07:00 +0200

    añadimos level2_11.py con cambios del profesor

--- level2_11.py
+++ level2_11.py
@@ -1,5 +1,20 @@
 ## Ejercicio 11
-def valores_atip(*numeros):
-    media = 0
-    count = 0
+from statistics import mean, stdev
+l = [1,2,3,4,5,6,7,8,9,10,100,1999]
+media = mean(l)
+
+def valores_atip(l):
+    media = mean(l)
+    desvs = stdev(l)
+
+    def score(n):
+        puntuacion = (n-media) / desvs
+        return puntuacion<-3 or puntuacion>3    
+    return score
     
+
+def resol(L):
+    return list(filter(valores_atip(L), L))
+
+
+print(resol(l))
