de20abe0f4d1bfca6ff29b5541c0378e7a1cbc66
AleValleLazaro.prueba <alexvalaz@gmail.com>
2024-06-17 09:23:41 +0200

    añadimos ejercicio71.py y cambios de archivos relacionados con el ejercicio

--- ejercicio71.py
+++ ejercicio71.py
@@ -2,7 +2,7 @@
 
 def multp():
     numero = int(input("Número entero del 1 al 10: "))
-    with open(f"tabla-{numero}.txt", 'w', encoding='utf8') as f:
+    with open(f"salidas\\tabla-{numero}.txt", 'w', encoding='utf8') as f:
         for i in range(0, 11):
             f.write(f"{numero}x{i}= {numero*i}\n")
 
